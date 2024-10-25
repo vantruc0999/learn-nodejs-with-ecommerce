3807240451b109375b459b190a42abb128caa198
vantruc0999 <vantruc0409@gmail.com>
2024-10-25 17:48:15 +0700

    refactor: redifine model to clarify schema

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -5,30 +5,43 @@ const { model, Schema } = require("mongoose");
 const DOCUMENT_NAME = "Order";
 const COLLECTION_NAME = "Orders";
 
+
+const itemProductSchema = new Schema(
+    {
+        price: { type: Number },
+        quantity: { type: Number },
+        productId: { type: String },
+    },
+    { _id: false }
+);
+
+
+const orderProductSchema = new Schema(
+    {
+        shopId: { type: String },
+        shopDiscounts: [
+            {
+                discountId: { type: String },
+                codeId: { type: String },
+            },
+        ],
+        priceRaw: { type: Number },
+        priceApplyDiscount: { type: Number },
+        itemProducts: [itemProductSchema],
+    },
+    { _id: false }
+);
+
 const orderSchema = new Schema(
     {
         orderUserId: { type: Number, required: true },
         orderCheckout: { type: Object, default: {} },
-        /*
-            order_checkout = {
-                totalPrice,
-                totalApplyDiscount,
-                freeShip
-            }
-        */
-        orderShipping: {
-            type: Object, default: {},
-            /*
-                street,
-                city,
-                state,
-                country
-            */
-        },
+        orderShipping: { type: Object, default: {} },
         orderPayment: { type: Object, default: {} },
-        orderProducts: { type: Array, required: true },
+        orderProducts: { type: [orderProductSchema] },
         orderTrackingNumber: {
-            type: String, default: '#000022102024'
+            type: String,
+            default: '#000022102024'
         },
         orderStatus: {
             type: String,
