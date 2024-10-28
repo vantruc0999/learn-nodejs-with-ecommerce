ae7e0452240de0134b5709a0a2d0fa46556a2f82
vantruc0999 <vantruc0409@gmail.com>
2024-10-28 22:41:14 +0700

    init user, resource, role model

---
+++ src/models/role.model.js
@@ -0,0 +1,29 @@
+"use strict"
+
+const { model, Schema } = require("mongoose");
+
+const DOCUMENT_NAME = "Role";
+const COLLECTION_NAME = "Roles";
+
+// const grantList = [
+//     { role: 'admin', resource: 'profile', action: 'read:own', attributes: '*' }
+// ]
+
+const roleSchema = new Schema(
+    {
+        roleName: { type: String, default: 'user', enum: ['user', 'shop', 'admin'] },
+        roleSlug: { type: String, required: true },
+        roleStatus: { type: String, default: 'active', enum: ['active', 'inactive', 'pending'] },
+        roleDescription: { type: String, default: '' },
+        roleGrants: {
+            resource: { type: Schema.Types.ObjectId, ref: 'Resource', required: true },
+            actions: { type: String, required: true },
+            attributes: { type: String, default: '*' }
+        }
+    }, {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+})
+
+module.exports = model(DOCUMENT_NAME, roleSchema);
+
