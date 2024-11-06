994884e3331795b41f601189afb4234e5aff6ef4
vantruc0999 <vantruc0409@gmail.com>
2024-11-06 09:20:58 +0700

    upload image using multer and cloudinary

---
+++ ecommerce-nodejs-api/src/controllers/upload.controller.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const { BadRequestError } = require("../core/error.response");
+const { SuccessResponse } = require("../core/success.response");
+const { uploadImageFromUrl, uploadImageFromLocal, uploadImageFromLocalFiles } = require("../services/upload.service");
+
+class UploadController {
+    uploadFile = async (req, res, next) => {
+        new SuccessResponse({
+            message: "Upload file successfully",
+            metadata: await uploadImageFromUrl()
+        }).send(res);
+    };
+
+    uploadFileThumb = async (req, res, next) => {
+        const { file } = req
+        if(!file){
+            throw new BadRequestError('File missing')
+        }
+        new SuccessResponse({
+            message: "Upload thumb successfully",
+            metadata: await uploadImageFromLocal({
+                path: file.path
+            })
+        }).send(res);
+    };
+
+    uploadImageFromLocalFiles = async (req, res, next) => {
+        const { files } = req
+        if(!files.length){
+            throw new BadRequestError('File missing')
+        }
+        new SuccessResponse({
+            message: "Upload thumb successfully",
+            metadata: await uploadImageFromLocalFiles({
+                files
+            })
+        }).send(res);
+    };
+}
+
+module.exports = new UploadController();
